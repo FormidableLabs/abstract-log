6d4fe9089b7a356b19b75f703555c25e4538afbb
Ryan Roemer <ryan@loose-bits.com>
2015-08-03 12:51:31 -0700

    More planning notes.

--- demo/app.js
+++ demo/app.js
@@ -8,10 +8,22 @@ console.log("TODO HI");
   - Input box for message
   - Select for instance
 
-  - `new AbstractLog({ shims: ["info", "error"] })`
+  - `new AbstractLog({ shims: ["info", "error", "foo"] })`
   - `new AbstractLog({ logger: console })` (And note ie9- wonkiness)
-  - `new AbstractLog({ logger: console, map: { foo: "info" }})`
+  - `new AbstractLog({ logger: console, map: { fatal: "error" }})`
   - `new AbstractLog({ logger: SimpleConsole })`
   - `new AbstractLog({ logger: bunyanInstance })`
+
+
+  victory-line/logger.js
+  ----------------------
+  module.exports = new AbstractLog( { shims: ["warn"] });
+
+  MY_APP_SERVER/config.js
+  ----------------
+  AbstractLog.setGlobal({
+    instance: bunyanLogger,
+    map: { warn: "warning" }
+  });
 
  */
